91e7492fdd8b80af53914b7e2d8f6443f2d9a951
mercaton <m.wieczorek001@wp.pl>
2023-08-26 14:39:47 +0200

    some another fixes in files structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import AddRecipe from "./components/AddRecipe.jsx";
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
+    <AddRecipe>
   </React.StrictMode>,
 )
