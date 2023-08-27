1bb6885b6d178acf3fe4f8a2fd003d1386d2fd78
mercaton <m.wieczorek001@wp.pl>
2023-08-27 21:48:13 +0200

    adding another scss file to project structure

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,6 @@ import AddRecipe from "./components/AddRecipe.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AddRecipe>
+
   </React.StrictMode>,
 )
