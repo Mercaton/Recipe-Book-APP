a68ba2c2f0265547f9ef886f20058dcec6918888
mercaton <m.wieczorek001@wp.pl>
2023-08-29 22:42:17 +0200

    creating base jsx structure in addrecipe component

--- src/components/AddRecipe.jsx
+++ src/components/AddRecipe.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function AddRecipe(props) {
+const AddRecipe = () => {
+    // Inicjalizujemy stan dla pól formularza
+    const [title, setTitle] = useState('');
+    const [ingredients, setIngredients] = useState('');
+    const [instructions, setInstructions] = useState('');
+
+    // Funkcja obsługująca przesłanie formularza
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Tutaj możesz wykorzystać dane z formularza, np. przesłać do bazy danych
+        console.log('Tytuł: ', title);
+        console.log('Składniki: ', ingredients);
+        console.log('Instrukcje: ', instructions);
+
+        // Czyszczenie pól formularza po wysłaniu
+        setTitle('');
+        setIngredients('');
+        setInstructions('');
+    };
+
     return (
-        <>
-
-        </>
+        <div>
+            <h2 className="addrecipe-hdl">Dodaj nowy przepis</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Tytuł:
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        required
+                    />
+                </label>
+                <label>
+                    Składniki:
+                    <textarea
+                        value={ingredients}
+                        onChange={(e) => setIngredients(e.target.value)}
+                        required
+                    />
+                </label>
+                <label>
+                    Instrukcje:
+                    <textarea
+                        value={instructions}
+                        onChange={(e) => setInstructions(e.target.value)}
+                        required
+                    />
+                </label>
+                <button type="submit">Dodaj Przepis</button>
+            </form>
+        </div>
     );
-}
+};
 
 export default AddRecipe;
