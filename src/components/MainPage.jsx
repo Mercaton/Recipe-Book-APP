02679e07a47f5982eeceec11835505dfff344c0f
mercaton <m.wieczorek001@wp.pl>
2023-09-11 21:03:47 +0200

    making recipe collection on main-page section

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -6,10 +6,10 @@ function MainPage() {
         <>
             <section className="main-page">
                 <div className="recipe-list">
-                    <div className="recipe-details"><img src="/src/assets/images/Daily-Dish-5-1030x1023.jpg" alt=""/><h3 className="recipe-title">Jajko sadzone</h3></div>
+                    <a href=""><div className="recipe-details"><img src="/src/assets/images/Daily-Dish-5-1030x1023.jpg" alt=""/><h3 className="recipe-title">Jajko sadzone</h3></div></a>
                     <div className="recipe-details"><img src="/src/assets/images/pumpkin-curry-soup-recipe.jpg" alt=""/><h3 className="recipe-title">Zupa dyniowa z mlekiem kokosowym</h3></div>
-                    <div className="recipe-details"><img src="" alt=""/><h3 className="recipe-title"></h3></div>
-                    <div className="recipe-details"><img src="" alt=""/><h3 className="recipe-title"></h3></div>
+                    <div className="recipe-details"><img src="" alt=""/><h3 className="recipe-title">Sałatka z tuńczykiem</h3></div>
+                    <div className="recipe-details"><img src="" alt=""/><h3 className="recipe-title">Tiramisu</h3></div>
                 </div>
             </section>
 
