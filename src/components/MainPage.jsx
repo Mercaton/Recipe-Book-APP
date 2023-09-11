705b6ed7ceb4ca091a435674d6edf51cd2a54cf3
mercaton <m.wieczorek001@wp.pl>
2023-09-11 15:04:24 +0200

    creating recipe details window structure

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -6,7 +6,7 @@ function MainPage() {
         <>
             <section className="main-page">
                 <div className="recipe-list">
-                    <div className="recipe-details"><img src="" alt=""/><h3 className="recipe-title"></h3></div>
+                    <div className="recipe-details"><img src="/src/assets/images/Daily-Dish-5-1030x1023.jpg" alt=""/><h3 className="recipe-title">Jajko sadzone</h3></div>
                     <div className="recipe-details"><img src="" alt=""/><h3 className="recipe-title"></h3></div>
                     <div className="recipe-details"><img src="" alt=""/><h3 className="recipe-title"></h3></div>
                 </div>
