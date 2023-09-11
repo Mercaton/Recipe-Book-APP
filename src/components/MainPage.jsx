2b75cf26996ccc63c1b6d9c1f7577eeb40943d33
mercaton <m.wieczorek001@wp.pl>
2023-09-11 20:23:49 +0200

    add recipe btn hover effects

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -7,7 +7,7 @@ function MainPage() {
             <section className="main-page">
                 <div className="recipe-list">
                     <div className="recipe-details"><img src="/src/assets/images/Daily-Dish-5-1030x1023.jpg" alt=""/><h3 className="recipe-title">Jajko sadzone</h3></div>
-                    <div className="recipe-details"><img src="" alt=""/><h3 className="recipe-title"></h3></div>
+                    <div className="recipe-details"><img src="/src/assets/images/pumpkin-curry-soup-recipe.jpg" alt=""/><h3 className="recipe-title">Zupa dyniowa z mlekiem kokosowym</h3></div>
                     <div className="recipe-details"><img src="" alt=""/><h3 className="recipe-title"></h3></div>
                     <div className="recipe-details"><img src="" alt=""/><h3 className="recipe-title"></h3></div>
                 </div>
