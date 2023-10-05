09e6ceb026eab8880dba701080c6ecdea6230a51
mercaton <m.wieczorek001@wp.pl>
2023-10-05 23:31:04 +0200

    making things work

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -31,10 +31,10 @@ function MainPage() {
         <>
             <section className="main-page">
                 <div className="recipe-list">
-                    <a id="openPopup" href="/details"><div className="recipe-details"><img src="/src/assets/images/Daily-Dish-5-1030x1023.jpg" alt=""/><h3 className="recipe-title">Jajko sadzone</h3></div></a>
-                    <div className="recipe-details"><img src="/src/assets/images/pumpkin-curry-soup-recipe.jpg" alt=""/><h3 className="recipe-title">Zupa dyniowa z mlekiem kokosowym</h3></div>
-                    <div className="recipe-details"><img src="/src/assets/images/Tuna-Pasta-Salad-3-683x1024.jpg" alt=""/><h3 className="recipe-title">Sałatka z tuńczykiem</h3></div>
-                    <div className="recipe-details"><img src="/src/assets/images/tiramisu-3.jpg" alt=""/><h3 className="recipe-title">Tiramisu</h3></div>
+                    <a href="/details"><div className="recipe-details"><img src="/src/assets/images/Daily-Dish-5-1030x1023.jpg" alt=""/><h3 className="recipe-title">Jajko sadzone</h3></div></a>
+                    <a href="/details"><div className="recipe-details"><img src="/src/assets/images/pumpkin-curry-soup-recipe.jpg" alt=""/><h3 className="recipe-title">Zupa dyniowa z mlekiem kokosowym</h3></div></a>
+                    <a href="/details"><div className="recipe-details"><img src="/src/assets/images/Tuna-Pasta-Salad-3-683x1024.jpg" alt=""/><h3 className="recipe-title">Sałatka z tuńczykiem</h3></div></a>
+                    <a href="/details"><div className="recipe-details"><img src="/src/assets/images/tiramisu-3.jpg" alt=""/><h3 className="recipe-title">Tiramisu</h3></div></a>
                 </div>
             </section>
 
