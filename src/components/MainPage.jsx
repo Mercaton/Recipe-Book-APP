abb19b6b8683a75a8f5c763ad7b1e1087e8ce4a3
mercaton <m.wieczorek001@wp.pl>
2023-10-05 23:24:02 +0200

    looking for recipe details popup window solution...

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,6 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
+// const openPopupButton = document.getElementById('openPopup');
+// const closePopupButton = document.getElementById('closePopup');
+// const popup = document.getElementById('popup');
+//
+// openPopupButton.addEventListener('click', () => {
+//     popup.style.display = 'block';
+// });
+//
+// closePopupButton.addEventListener('click', () => {
+//     popup.style.display = 'none';
+// });
+
 function MainPage() {
     const [recipes, setRecipes] = useState([]);
 
@@ -17,7 +29,7 @@ function MainPage() {
         <>
             <section className="main-page">
                 <div className="recipe-list">
-                    <a href="/details"><div className="recipe-details"><img src="/src/assets/images/Daily-Dish-5-1030x1023.jpg" alt=""/><h3 className="recipe-title">Jajko sadzone</h3></div></a>
+                    <a id="openPopup" href="/details"><div className="recipe-details"><img src="/src/assets/images/Daily-Dish-5-1030x1023.jpg" alt=""/><h3 className="recipe-title">Jajko sadzone</h3></div></a>
                     <div className="recipe-details"><img src="/src/assets/images/pumpkin-curry-soup-recipe.jpg" alt=""/><h3 className="recipe-title">Zupa dyniowa z mlekiem kokosowym</h3></div>
                     <div className="recipe-details"><img src="/src/assets/images/Tuna-Pasta-Salad-3-683x1024.jpg" alt=""/><h3 className="recipe-title">Sałatka z tuńczykiem</h3></div>
                     <div className="recipe-details"><img src="/src/assets/images/tiramisu-3.jpg" alt=""/><h3 className="recipe-title">Tiramisu</h3></div>
