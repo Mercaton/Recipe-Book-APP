2950e284769d6e6a053e490a92cb654c8c1f329e
mercaton <m.wieczorek001@wp.pl>
2023-10-09 22:08:17 +0200

    trying to setup server.js file

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Recipe = require('../models/RecipeSchema');
+const Recipe = require('.//RecipeSchema');
 
 
 
--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function RecipeDetails() {
+function RecipeDetails({ recipe, onClose }) {
     return (
         <>
             <h1> Hello World </h1>
