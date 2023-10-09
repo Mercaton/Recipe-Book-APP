c650286e32f14c791a7290a7f0e5d8fcd79b0487
mercaton <m.wieczorek001@wp.pl>
2023-10-09 23:42:39 +0200

    creating routes for crud service

--- server/RecipeController.jsx
+++ server/RecipeController.jsx
@@ -10,12 +10,34 @@ exports.getAllRecipes = async (req, res) => {
     }
 };
 
-export.getRecipeById = asyns (req, res) => {
+exports.getRecipeById = async (req, res) => {
     const {id} = req.params;
     try {
         const recipe = await Recipe.findById(id);
         if (!recipe) {
-            return res.status(404).json({error: 'Przepis nie został znaleziony'})
+            return res.status(404).json({error: 'Przepis nie został znaleziony'});
         }
+        res.json(recipe);
+    } catch (error) {
+        console.error('Błąd podczas pobierania przepisu', error);
+        res.status(500).json({error: 'Wystąpił błąd podczas pobierania przepisu'});
     }
-}
+};
+
+exports.createRecipe = async (req, res) => {
+    const {title, ingredients, instructions, image} = req.body;
+    try {
+        const newRecipe = new Recipe({
+            title,
+            ingredients,
+            instructions,
+            image,
+        });
+        const savedRecipe = await newRecipe.save();
+        res.status(201).json(savedRecipe);
+    } catch (error) {
+        console.error('Błąd podczas zapisywania przepisu', error);
+        res.status(500).json({error: 'Wystąpił błąd podczas zapisywania przepisu'});
+    }
+};
+
